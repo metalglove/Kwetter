ee4cdb9a712f29b7c367c224cc1a62027a107ed0
Mario Lucassen <Mario.lucassen@hotmail.com>
2021-04-24 01:02:59 +0200

    more logging

--- src/Services/AuthorizationService/Infrastructure/Services/AuthorizationService.cs
+++ src/Services/AuthorizationService/Infrastructure/Services/AuthorizationService.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -41,30 +42,49 @@ namespace Kwetter.Services.AuthorizationService.Infrastructure.Services
         /// <inheritdoc cref="IAuthorizationService.AuthorizeAsync(string)"/>
         public async Task<AuthorizationDto> AuthorizeAsync(string code)
         {
-            HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, _authorizationConfiguration.TokenUri);
-            string codeQuery = 
-                $"code={code}" +
-                $"&client_id={_authorizationConfiguration.ClientId}" +
-                $"&client_secret={_authorizationConfiguration.ClientSecret}" +
-                $"&redirect_uri=postmessage" +
-                $"&grant_type=authorization_code";
-            HttpContent httpContent = new StringContent(codeQuery, Encoding.UTF8, "application/x-www-form-urlencoded");
-            httpRequestMessage.Content = httpContent;
-            HttpResponseMessage httpResponseMessage = await _httpClient.SendAsync(httpRequestMessage);
-            string json = await httpResponseMessage.Content.ReadAsStringAsync();
+            try
+            {
+                HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, _authorizationConfiguration.TokenUri);
+                string codeQuery =
+                    $"code={code}" +
+                    $"&client_id={_authorizationConfiguration.ClientId}" +
+                    $"&client_secret={_authorizationConfiguration.ClientSecret}" +
+                    $"&redirect_uri=postmessage" +
+                    $"&grant_type=authorization_code";
+                _logger.LogInformation($"codeQuery: {codeQuery}");
+                HttpContent httpContent = new StringContent(codeQuery, Encoding.UTF8, "application/x-www-form-urlencoded");
+                httpRequestMessage.Content = httpContent;
+                _logger.LogInformation("About to send request to google!");
+                HttpResponseMessage httpResponseMessage = await _httpClient.SendAsync(httpRequestMessage);
+                _logger.LogInformation("Received response from google!");
+                _logger.LogInformation("Reading json from response 1....");
+                using (StreamReader reader = new(await httpResponseMessage.Content.ReadAsStreamAsync()))
+                {
+                    string response = reader.ReadToEnd();
+                    _logger.LogInformation(response);
+                }
 
-            StringBuilder builder = new(Environment.NewLine);
-            builder.AppendLine($"JSON:{Environment.NewLine}{json}");
-            builder.AppendLine($"HTTP StatusCode:{Environment.NewLine}{httpResponseMessage.StatusCode}");
-            builder.AppendLine($"HTTP ReasonPhrase:{Environment.NewLine}{httpResponseMessage.ReasonPhrase}");
-            builder.AppendLine($"HTTP Response headers:");
-            foreach (KeyValuePair<string, IEnumerable<string>> header in httpResponseMessage.Headers)
-                builder.AppendLine($"\t{header.Key}:{string.Join(',', header.Value)}");
-            _logger.LogInformation(builder.ToString());
+                _logger.LogInformation("Reading json from response 2....");
+                string json = await httpResponseMessage.Content.ReadAsStringAsync();
 
-            httpResponseMessage.EnsureSuccessStatusCode();
-            AuthorizationDto authorizationDto = JsonSerializer.Deserialize<AuthorizationDto>(json);
-            return authorizationDto;
+                StringBuilder builder = new(Environment.NewLine);
+                builder.AppendLine($"JSON:{Environment.NewLine}{json}");
+                builder.AppendLine($"HTTP StatusCode:{Environment.NewLine}{httpResponseMessage.StatusCode}");
+                builder.AppendLine($"HTTP ReasonPhrase:{Environment.NewLine}{httpResponseMessage.ReasonPhrase}");
+                builder.AppendLine($"HTTP Response headers:");
+                foreach (KeyValuePair<string, IEnumerable<string>> header in httpResponseMessage.Headers)
+                    builder.AppendLine($"\t{header.Key}:{string.Join(',', header.Value)}");
+                _logger.LogInformation(builder.ToString());
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+                AuthorizationDto authorizationDto = JsonSerializer.Deserialize<AuthorizationDto>(json);
+                return authorizationDto;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
     }
 }
